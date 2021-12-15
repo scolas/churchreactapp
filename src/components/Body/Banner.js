cf6e97fe4e25301df4fa291e5f34ca3b0813e478
scolas <scolas481@gmail.com>
2021-12-14 22:51:18 -0500

    clean up

--- src/components/Body/Banner.js
+++ src/components/Body/Banner.js
@@ -10,29 +10,7 @@ render (){
         <img src={backgrounds} alt="Logo" />
         <br/>
     </div>
-    
-/*<div className="h-screen box-border border-4"  style={{ background: `url(${backgrounds})`, }}>
-    <div className=" p-4  ">
-        <div className="churchName">
-            <h1 className="font-semibold text-blue-500 text-7xl">L'EGLISE PENTECOTE DE LA NOUVELLE GENERATION</h1>
-        </div>
-        <br/><br/>
-        <div className=" text-blue-500 text-4xl pentecostal">
-            Pentecostal Church of The New Generation
-            <hr className=" text-red-500"/>
-        </div>
-        
-        <br/><br/><br/>
-        <div className="text-blue-500 text-4xl address">
-            Pastor John & Jacqueline Celestin
-        </div>
-        
-        <div className="text-blue-500 text-4xl address">
-            Phone number (203) 450 - 8861
-        </div>
-    </div>
-
-</div>*/);
+    );
     }
 
 
